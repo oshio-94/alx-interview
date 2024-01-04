e810f4c8bf56cfdd1b642bda879fe3568d64b592
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-04 11:04:44 +0100

    New task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# iterarte upto n
+def pascal_triangle(n) {
+if (n <=0){
+    return [];
+} else{
+    for i in range(n):
+        # adjust space
+        print(' '*(n-i), end='')
+        # compute power of 11
+	    print(' '.join(map(str, str(11**i))))
+}
+};
