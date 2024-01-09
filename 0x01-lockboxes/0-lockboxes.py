9670cd571d305456a92fbd11602f9f3b7f9422f5
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-09 20:36:18 +0100

    python task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" method that determines if all the boxes can be opened."""
+""" method to determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened."""
-    sizebox = len(boxes)  # sizebox of the list of boxes.
-    newbox = {}  # dictionary that will contain the boxes that can be opened.
-    check = 0  # check of the box that will be checked.
+    """ method to determines if all the boxes can be opened."""
+    sizebox = len(boxes) 
+    newbox = {} 
+    check = 0  
 
-    for keys in boxes:  # for each box in the list of boxes.
+    for keys in boxes: 
         if len(keys) == 0 or check == 0:
             newbox[check] = -1
         for key in keys:
             if key < sizebox and key != check:
-                newbox[key] = key  # the box is added to the dictionary.
+                newbox[key] = key 
         if len(newbox) == sizebox:
-            return True  # all the boxes can be opened.
+            return True  
         check += 1
     return False
