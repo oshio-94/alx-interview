decf6342a22ab79e9cbe4fef10bea2ea19d0e999
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-17 20:40:32 +0100

    New Project

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+    """
+    write a method that calculates
+    the fewest number of operations needed to result in exactly n H characters
+    in the file
+    """
+    if n <= 1:
+        return 0
+    minnum, rem, numOfOperations = n, 2, 0
+
+    while minnum > 1:
+        if minnum % rem == 0:
+            minnum = minnum / rem
+            numOfOperations = numOfOperations + rem
+        else:
+            rem += 1
+    return numOfOperations
