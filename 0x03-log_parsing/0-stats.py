72e85044bfe5814f57337ee4c2220b5fe1b9683e
John Ikhanoba <oshioi@yahoo.co.uk>
2024-01-25 14:41:17 +0100

    new task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,45 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
 
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
-status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-size = 0
+
+def printstatus_code(lst, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(lst.keys()):
+        if lst[i] != 0:
+            print("{}: {:d}".format(i, lst[i]))
+
+
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
 count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        lst = line.split(" ")
-        if len(lst) > 4:
-            code = lst[-2]
-            size = int(lst[-1])
-            if code in status_code.keys():
-                status_code[code] += 1
-            size += size
-            count += 1
+        if count != 0 and count % 10 == 0:
+            printstatus_code(status_code, size)
 
-        if count == 10:
-            count = 0
-            print('File size: {}'.format(size))
-            for key, value in sorted(status_code.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        stlist = line.split()
+        count += 1
 
-except Exception as err:
-    pass
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
 
-finally:
-    print('File size: {}'.format(size))
-    for key, value in sorted(status_code.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+        try:
+            if stlist[-2] in status_code:
+                status_code[stlist[-2]] += 1
+        except:
+            pass
+    printstatus_code(status_code, size)
+
+
+except KeyboardInterrupt:
+    printstatus_code(status_code, size)
+    raise
