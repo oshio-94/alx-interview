fec196ec94cdc6c0e66163bb095f17307dd3341b
John Ikhanoba <oshioi@yahoo.co.uk>
2024-02-13 19:11:48 +0100

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,7 @@ request(url, function (err, response, body) {
   if (err == null) {
     const resp = JSON.parse(body);
     const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {cd 
+    for (let i = 0; i < characters.length; i++) {
       request(characters[i], function (err, response, body) {
         if (err == null) {
           console.log(JSON.parse(body).name);
