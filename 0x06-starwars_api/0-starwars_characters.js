017d6d00206d4adc31fbb1bd0b457c1240728fc6
John Ikhanoba <oshioi@yahoo.co.uk>
2024-02-14 09:13:30 +0000

    updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
+request(url, async (err, response, body) => {
   if (err == null) {
     const resp = JSON.parse(body);
     const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+    for (let character of characters) {
+	    await new Promise((resolve, reject) => {
+		    request(character, function (err, response, body) {
+			    if (err == null) {
+				    console.log(JSON.parse(body).name);
+				    resolve();
+			    }
+		    });
+	    });
     }
   }
 });
